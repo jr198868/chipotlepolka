156c30c15a3a7c2e0f11959977b6e88376dc3c68
Raymond <rjingraymond88@gmail.com>
2022-04-17 11:54:17 -0400

    add functioins

--- chipotlepolka.py
+++ chipotlepolka.py
@@ -3,12 +3,26 @@ import csv
 #parse csv file
 save_list = []
 
-def parse_csv():
-    with open () as f:
-        for line in f:
-            save_list.append(line.strip().split(','))
+class parse_csv:
+    
+    def read_csv(csv_file):
+        with open (csv_file) as f:
+            for line in f:
+                save_list.append(line.strip().split(','))
+
+        save_list[0] = [save_list[0][0].split('\ufeff')[1]] + save_list[0][1:]
+
+        print('rows:'+ str(len(save_list)) + ',' + 'columns:' + str(len(save_list[0])))
+        return save_list
 
 
-    return save_list
 
 
+
+class savedata:
+
+    #save list to csv
+    def savetolist(path, target_list):
+        with open (path, 'w') as f:
+            wr = csv.writer(f)
+            wr.writerows(target_list)
