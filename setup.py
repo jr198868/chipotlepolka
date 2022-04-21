e7100df5d4b7559c342aa6d2018557d83839c01e
Raymond <rjingraymond88@gmail.com>
2022-04-21 09:58:24 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'chipotlepolka',
+    version = '0.1.1',
+    keywords='Data science, Data engineering, Data cleanup',
+    description = 'a library for DS and DE',
+    license = 'MIT License',
+    url = 'https://github.com/jr198868/chipotlepolka',
+    author = 'Raymond Jing',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
